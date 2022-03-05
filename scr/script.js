7c53c58f80adc5e5066587035938a32a57bc3c1e
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2022-03-05 14:02:30 -0800

    Update script.js

--- scr/script.js
+++ scr/script.js
@@ -1,12 +1,12 @@
 function updateClock() {
-	const date = new Date();
-	const hours = date.getHours();
-	const minutes = date.getMinutes();
-	const ampm = hours >= 12 ? 'pm' : 'am';
+	let date = new Date();
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+	let ampm = hours >= 12 ? 'pm' : 'am';
 	hours = hours % 12;
 	hours = hours ? hours : 12;
 	minutes = minutes < 10 ? '0'+minutes : minutes;
-	const time = hours + ':' + minutes + ' ' + ampm;
+	let time = hours + ':' + minutes + ' ' + ampm;
 	document.getElementById('time').innerHTML = time;
 	setTimeout(updateClock, 1000);
 }
